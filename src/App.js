d3fd02deed5b666684c690517c8ae4c70890f646
Uday Alla <uday@fullstory.com>
2023-03-15 09:37:25 -0400

    integrate fs browser sdk into Sample Electron App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import ReactWeather, { useOpenWeather } from 'react-open-weather';
+import React, { useEffect } from 'react';
+import * as FullStory from '@fullstory/browser';
 import './App.css';
 
+
+
 function App() {
+  useEffect(() => {
+    FullStory.init({ orgId: '<Replace with your orgID>' });
+
+  }, []);
+
   const { data, isLoading, errorMessage } = useOpenWeather({
     key: '4cf55dfa9a0ab63a939fffb8ae325498',
     lat: '1.2921',
